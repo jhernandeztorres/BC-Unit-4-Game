9112eb62d9114c4a3c374147adfd276fa39f99d0
Jose Hernandez Torres <jose@hernandezt.com>
2018-11-07 18:09:28 -0700

    Images added

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+$("img").on("click", function(){
+        // first image clicked moves to fighter area
+        
+        // second image clicked goes to defender area
+    
+        // other images are hidden until someone is defeated
+    
+})
+
+
+
